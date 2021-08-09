1072b4a92347df830e00ae96497469fb7783df19
Eamonn de Leastar <edeleastar@wit.ie>
2021-08-09 09:20:52 +0100

    correct algorithm

--- utils/playlist-analytics.js
+++ utils/playlist-analytics.js
@@ -6,7 +6,9 @@ const playlistAnalytics = {
     if (playlist.songs.length > 0) {
       shortestSong = playlist.songs[0];
       for (let i = 1; i < playlist.songs.length; i++) {
-        shortestSong = playlist.songs[i];
+        if (playlist.songs[i].duration < shortestSong.duration) {
+          shortestSong = playlist.songs[i];
+        }
       }
     }
     return shortestSong;
